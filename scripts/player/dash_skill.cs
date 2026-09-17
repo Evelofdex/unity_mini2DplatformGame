21ff9050c29b41a48dbf9232a91cd50dcbda96d4
Evelofdex <evelofdex@gmail.com>
2026-09-17 15:14:02 +0700

    dash skill fix

--- scripts/player/dash_skill.cs
+++ scripts/player/dash_skill.cs
@@ -6,21 +6,18 @@ using UnityEngine;
 public class dash_skill : MonoBehaviour
 {
     public bool isDashing;
-    [SerializeField] private float dashDuration;
-    private float dashCooldownDuration = 1f;
+    private float dashDuration = 0.3f;
+    private float dashCooldownDuration = 3f;
     private bool isCooldown_dash;
 
     private Rigidbody2D rb;
 
-    [SerializeField] float dashForce;
+    float dashForce = 20f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         isCooldown_dash = false;
-
-        //
-        dashForce = 10f;
     }
 
     // Update is called once per frame
@@ -28,38 +25,40 @@ public class dash_skill : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.A) && !isCooldown_dash)
         {
-            StartCoroutine(dashLeft());
+            StartCoroutine(dashLeft(dashDuration));
             StartCoroutine(dashCooldown(dashCooldownDuration));
         }
         if(Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.D) && !isCooldown_dash)
         {
-           StartCoroutine(dashRight());
+           StartCoroutine(dashRight(dashDuration));
            StartCoroutine(dashCooldown(dashCooldownDuration));
         }
     }
 
-    IEnumerator dashLeft()
+    IEnumerator dashLeft(float duration)
     {
         rb.AddForce(Vector2.left * dashForce, ForceMode2D.Impulse);
-        Debug.Log("dashing");
+        // Debug.Log("dashing");
         isDashing = true;   
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(duration);
         isDashing = false;  
         isCooldown_dash = true;
     }
-    IEnumerator dashRight()
+    IEnumerator dashRight(float duration)
     {
         rb.AddForce(Vector2.right * dashForce, ForceMode2D.Impulse);
-        Debug.Log("dashing");
+        // Debug.Log("dashing");
         isDashing = true;
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(duration);
         isDashing = false;
         isCooldown_dash = true;
     }
 
     IEnumerator dashCooldown(float cooldown)
     {
+        Debug.Log("dash cooldown");
         yield return new WaitForSeconds(cooldown);
         isCooldown_dash = false;
+        Debug.Log("dash cooldown ended");
     }
 }
