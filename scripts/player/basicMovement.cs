2a949d850144cbc9d6f1d21fdbe00c8fa5cb41f6
Evelofdex <evelofdex@gmail.com>
2026-09-17 13:36:17 +0700

    jump, fall fixing

--- scripts/player/basicMovement.cs
+++ scripts/player/basicMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class basicMovement : MonoBehaviour
@@ -8,7 +9,11 @@ public class basicMovement : MonoBehaviour
     private Rigidbody2D rb;
    
 
-    [SerializeField] private float spd;
+    [SerializeField] private float spd = 6f;
+    //jump mechanics
+    [SerializeField] private float jumpForce;
+    private bool isGrounded = true;
+    [SerializeField] private float fallMultiplier;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +21,8 @@ public class basicMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         //
-        spd = 5f;
-  
+        jumpForce = 5f;
+        fallMultiplier = 2f;
     }
 
 
@@ -28,14 +33,38 @@ public class basicMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) direction += Vector2.right;
         Vector2 moveDirection = new Vector2(direction.x * spd, rb.velocity.y);
         //fall Multiplier
-        
-
-        rb.velocity = moveDirection;
+        if (!isGrounded && rb.velocity.y < 0)
+        {
+            rb.velocity = moveDirection + (Vector2.up *  (fallMultiplier * -1)); 
+        }
+        else
+        {
+            rb.velocity = moveDirection; 
+        }
+        Debug.Log(rb.velocity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            // Debug.Log("Ground Collision");
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            // Debug.Log("Ground Exit");
+            isGrounded = false;
+        }
     }
 }
